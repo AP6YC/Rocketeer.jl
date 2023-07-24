92fc2ed43543f04e830cebda7306930dec89ae60
Sasha Petrenko <sap625@umsystem.edu>
2023-07-24 11:06:44 -0500

    Add DSE templates

--- src/lib/lib.jl
+++ src/lib/lib.jl
@@ -15,6 +15,8 @@ Aggregates all types and functions that are used throughout the `OAR` project.
 # Project version constant
 include("version.jl")
 
+# DocStringExtensions templates, etc.
+include("docstrings.jl")
+
 # Definition of the Rocket method
 include("rocket.jl")
-
